f13288df97a785e77f88a635f674d9977ea91c60
pppplin <wz723@nyu.edu>
2018-04-18 14:33:01 +0800

    ops/causal_conv add velocity option

--- wavenet/ops.py
+++ wavenet/ops.py
@@ -24,38 +24,63 @@ optimizer_factory = {'adam': create_adam_optimizer,
                      'rmsprop': create_rmsprop_optimizer}
 
 
-def time_to_batch(value, dilation, name=None):
+def time_to_batch(value, dilation, velocity=False, name=None):
     with tf.name_scope('time_to_batch'):
         shape = tf.shape(value)
         pad_elements = dilation - 1 - (shape[1] + dilation - 1) % dilation
-        padded = tf.pad(value, [[0, 0], [0, pad_elements], [0, 0]])
-        reshaped = tf.reshape(padded, [-1, dilation, shape[2]])
-        transposed = tf.transpose(reshaped, perm=[1, 0, 2])
-        return tf.reshape(transposed, [shape[0] * dilation, -1, shape[2]])
+        if velocity:
+            padded = tf.pad(value, [[0, 0], [0, pad_elements],
+                [0, 0], [0, 0]])
+            reshaped = tf.reshape(padded, [-1, dilation, 2, shape[-1]])
+            transposed = tf.transpose(reshaped, perm=[1, 0, 2, 3])
+            return tf.reshape(transposed, [shape[0]*dilation, -1, 2, shape[-1]])
+        else:
+            padded = tf.pad(value, [[0, 0], [0, pad_elements], [0, 0]])
+            reshaped = tf.reshape(padded, [-1, dilation, shape[2]])
+            transposed = tf.transpose(reshaped, perm=[1, 0, 2])
+            return tf.reshape(transposed, [shape[0] * dilation, -1, shape[2]])
 
 
-def batch_to_time(value, dilation, name=None):
+def batch_to_time(value, dilation, velocity=False, name=None):
     with tf.name_scope('batch_to_time'):
         shape = tf.shape(value)
-        prepared = tf.reshape(value, [dilation, -1, shape[2]])
-        transposed = tf.transpose(prepared, perm=[1, 0, 2])
-        return tf.reshape(transposed,
+        if velocity:
+            prepared = tf.reshape(value, [dilation, -1, 2, shape[-1]])
+            transposed = tf.transpose(prepared, perm=[1, 0, 2, 3])
+            return tf.reshape(transposed,
+                            [tf.div(shape[0], dilation), -1, 2, shape[-1]])
+        else:
+            prepared = tf.reshape(value, [dilation, -1, shape[2]])
+            transposed = tf.transpose(prepared, perm=[1, 0, 2])
+            return tf.reshape(transposed,
                           [tf.div(shape[0], dilation), -1, shape[2]])
 
 
-def causal_conv(value, filter_, dilation, name='causal_conv'):
+def causal_conv(value, filter_, dilation, velocity=False, name='causal_conv'):
     with tf.name_scope(name):
         filter_width = tf.shape(filter_)[0]
         if dilation > 1:
             transformed = time_to_batch(value, dilation)
-            conv = tf.nn.conv1d(transformed, filter_, stride=1,
+            if velocity:
+                conv = tf.nn.conv2d(transformed, filter_, stride=1, padding='VALID')
+                restored = batch_to_time(conv, dilation)
+            else:
+                conv = tf.nn.conv1d(transformed, filter_, stride=1,
                                 padding='VALID')
-            restored = batch_to_time(conv, dilation)
+                restored = batch_to_time(conv, dilation)
         else:
-            restored = tf.nn.conv1d(value, filter_, stride=1, padding='VALID')
+            if velocity:
+                restored = tf.nn.conv2d(value, filter_, stride=1, padding='VALID')
+            else:
+                restored = tf.nn.conv1d(value, filter_, stride=1, padding='VALID')
         # Remove excess elements at the end.
         out_width = tf.shape(value)[1] - (filter_width - 1) * dilation
-        result = tf.slice(restored,
+        if velocity:
+            result = tf.slice(restored,
+                        [0, 0, 0, 0],
+                        [-1, out_width, -1, -1])
+        else:
+            result = tf.slice(restored,
                           [0, 0, 0],
                           [-1, out_width, -1])
         return result
